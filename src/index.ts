44d0f95dadc67471d937aef6f3fcfbf1241d2e86
Pedro Simeão <pedro.carvalho@criticalmanufacturing.com>
2017-05-17 15:58:18 +0100

    Add gulp support for piping

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,12 @@
 // ==================================================================
 // = GetText <--> i18n tool for generating POT and reading PO files =
 // ==================================================================
-import * as path from "path";
-import * as fs from "fs";
-
+import * as gulp from "./gulp";
 import { ParserFactory } from "./parsers/index";
-import { Package } from "./model/package";
 import { WriterFactory } from "./writers/index";
 
-let parser = ParserFactory.getParser(path.join(__dirname, ".."),
-    [
-        path.join(__dirname, "../test/mocks/multilevelExample/mock.default.ts"),
-        path.join(__dirname, "../test/mocks/multilevelExample/mock.pt-PT.ts")
-    ]
-);
-let pack = parser.run();
-
-let writer = WriterFactory.getWriter(pack, "pt-PT");
-let buffer = writer.run();
-let result = buffer[0].content.toString("utf-8");
-
-fs.writeFile("C:\\Temp\\i18n.po", buffer, () => {
-    console.log("File written to the disk!");
-
-        let parser = ParserFactory.getParser(path.join(__dirname, ".."),
-        [
-            path.join(__dirname, "../test/mocks/crossReferenceExample/mock.default.ts"),
-            path.join(__dirname, "../test/mocks/crossReferenceExample/mock.pt-PT.ts")
-        ]
-    );
-    let pack = parser.run();
-
-    let writer = WriterFactory.getWriter(pack, "pt-PT");
-    let buffer = writer.run();
-    let result = buffer[0].content.toString("utf-8");
-
-    fs.writeFile("C:\\Temp\\i18n.complex.po", buffer, () => {
-        console.log("File written to the disk!");
-        process.exit(0);
-    });
-    // process.exit(0);
-});
-
-
-// console.dir(pack);
+module.exports = {
+    parser: ParserFactory,
+    writer: WriterFactory,
+    gulp: gulp
+};
